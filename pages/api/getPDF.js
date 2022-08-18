a530e1410d8b8a4474b85a6ca58f88be2a063335
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2022-08-18 13:45:05 +0300

    Download pdf file url

---
+++ pages/api/getPDF.js
@@ -0,0 +1,19 @@
+import stream from "stream";
+import { promisify } from "util";
+import fetch from "node-fetch";
+
+const pipeline = promisify(stream.pipeline);
+const url =
+  "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
+
+const handler = async (req, res) => {
+  const response = await fetch(url);
+  if (!response.ok)
+    throw new Error(`unexpected response ${response.statusText}`);
+
+  res.setHeader("Content-Type", "application/pdf");
+  res.setHeader("Content-Disposition", "attachment; filename=dummy.pdf");
+  await pipeline(response.body, res);
+};
+
+export default handler;
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Download PDF</title>
+        <meta name="description" content="Download PDF - URL" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          <a href="/api/getPDF">Download PDF</a>
+        </p>
+      </main>
+    </div>
+  );
+}
